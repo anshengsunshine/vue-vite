ef3d4fe72bbb47a41c72e2d45233b5a0040530f0
jiajing <jiajing@zhiweidata.com>
2022-08-24 19:06:39 +0800

    初始化项目

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>这是 about 页面</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>这是 home 页面</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
